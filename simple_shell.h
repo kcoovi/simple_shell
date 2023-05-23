db03ecf1bbd8ee04c83393b776ede7bbea086c65
Diana Sungu <sungudiana1@gmail.com>
2023-05-23 16:17:02 -0700

    simple shell header file

--- simple_shell.h
+++ simple_shell.h
@@ -1,6 +1,21 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+#define WRITE_BUF_SIZE 1024
+#define READ_BUF_SIZE 1024
 #define MAX_INPUT_LENGTH 1024
 
 void execute_command(char *command);
