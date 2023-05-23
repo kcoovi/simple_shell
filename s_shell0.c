6e6efceff367065333946e206d358ee826da0ef3
Diana Sungu <sungudiana1@gmail.com>
2023-05-23 08:19:27 -0700

    creating a code tha's betty compliant

--- s_shell0.c
+++ s_shell0.c
@@ -1,14 +1,22 @@
+#include "simple_shell.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define MAX_INPUT_LENGTH 1024
+/**
+ * execute_command - Function that executes a command in process child
+ * @command: command to be executed.
+ * Return: 0
+ */
 
 void execute_command(char *command)
 {
 	pid_t pid = fork();
+
 	if (pid == -1)
+
 	{
 	perror("fork");
 	exit(EXIT_FAILURE);
@@ -18,6 +26,7 @@ void execute_command(char *command)
 	char *args[MAX_INPUT_LENGTH];
 	int i = 0;
 	char *token = strtok(command, " \t\n");
+
 	while (token)
 	{
 	args[i++] = token;
@@ -31,17 +40,21 @@ void execute_command(char *command)
 	exit(EXIT_FAILURE);
 	}
 	}
-	else 
+	else
 	{
 	wait(NULL);
 	}
 }
+/**
+ * main - A shell entry point.
+ * Return: 0.
+ */
 
 int main(void)
 {
 	char input[MAX_INPUT_LENGTH];
 
-	while (1) 
+	while (1)
 	{
 	printf("shell> ");
 	fflush(stdout);
